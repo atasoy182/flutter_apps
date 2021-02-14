c7ae42d0f29557544618c40245bd169be666bf96
mehmet berkay <atasoy182@gmail.com>
2021-02-14 23:05:08 +0300

    modeller eklendi, database helper eklendi.

---
+++ lib/utils/database_helper.dart
@@ -0,0 +1,67 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  // Singleton
+  static DatabaseHelper _databaseHelper;
+  static Database _database;
+
+  factory DatabaseHelper() {
+    if (_databaseHelper == null) {
+      _databaseHelper = DatabaseHelper._internal();
+      return _databaseHelper;
+    } else {
+      return _databaseHelper;
+    }
+  }
+
+  DatabaseHelper._internal();
+
+  Future<Database> _getDatabase() async {
+    if (_database == null) {
+      _database = await _initializeDatabase();
+      return _database;
+    } else {
+      return _database;
+    }
+  }
+
+  Future<Database> _initializeDatabase() async {
+    var databasesPath = await getDatabasesPath();
+    var path = join(databasesPath, "notlar.db");
+
+// Check if the database exists
+    var exists = await databaseExists(path);
+
+    if (!exists) {
+      // Should happen only the first time you launch your application
+      print("Creating new copy from asset");
+
+      // Make sure the parent directory exists
+      try {
+        await Directory(dirname(path)).create(recursive: true);
+      } catch (_) {}
+
+      // Copy from asset
+      ByteData data = await rootBundle.load(join("assets", "notlar.db"));
+      List<int> bytes =
+          data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+      // Write and flush the bytes written
+      await File(path).writeAsBytes(bytes, flush: true);
+    } else {
+      print("Opening existing database");
+    }
+    // open the database
+    return await openDatabase(path, readOnly: false);
+  }
+
+  Future<List<Map<String, dynamic>>> kategorileriGetir() async {
+    var db = await _getDatabase();
+    var sonuc = await db.query("kategori");
+    return sonuc;
+  }
+}
