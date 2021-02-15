1a39fbc32365a12e641a2206b238c758d6bcff53
mehmet berkay <atasoy182@gmail.com>
2021-02-16 00:49:13 +0300

    Database helper crud işlemleri için metotlar eklendi, görsel düzenlemeler yapıldı.

--- notsepeti/lib/main.dart
+++ notsepeti/lib/main.dart
@@ -4,20 +4,103 @@ void main() {
   runApp(HomePage());
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Center(
-        child: Text("Başlangıç Sayfası"),
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: NotListesi(),
+    );
+  }
+}
+
+class NotListesi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Not Sepeti"),
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => {
+              showDialog(
+                  context: context,
+                  //barrierDismissible: false,
+                  builder: (context) {
+                    return SimpleDialog(
+                      title: Text(
+                        "Kategori Ekle",
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      ),
+                      children: [
+                        Form(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                labelText: "Kategori Adı",
+                                border: OutlineInputBorder(),
+                              ),
+                              validator: (girilenKategoriAdi) {
+                                if (girilenKategoriAdi.length < 3) {
+                                  return "En az üç karekter giriniz !";
+                                } else {
+                                  return "";
+                                }
+                              },
+                            ),
+                          ),
+                        ),
+                        ButtonBar(
+                          children: [
+                            RaisedButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              color: Colors.orangeAccent,
+                              child: Text(
+                                "Vazgeç",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            RaisedButton(
+                              onPressed: () {},
+                              color: Colors.greenAccent,
+                              child: Text(
+                                "Kaydet",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    );
+                  })
+            },
+            mini: true,
+            tooltip: "Kategori Ekle",
+            child: Icon(Icons.add_circle),
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            tooltip: "Not Ekle",
+            child: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Container(
+        width: 100,
+        height: 100,
+        color: Colors.red,
       ),
     );
   }
--- notsepeti/lib/utils/database_helper.dart
+++ notsepeti/lib/utils/database_helper.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:crud_app/models/kategori.dart';
+import 'package:crud_app/models/notlar.dart';
 import 'package:flutter/services.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
@@ -59,9 +61,58 @@ class DatabaseHelper {
     return await openDatabase(path, readOnly: false);
   }
 
+  // Kategori işlemleri
   Future<List<Map<String, dynamic>>> kategorileriGetir() async {
     var db = await _getDatabase();
     var sonuc = await db.query("kategori");
+    return sonuc;
+  }
+
+  Future<int> kategoriEkle(Kategori kategori) async {
+    var db = await _getDatabase();
+    var sonuc = await db.insert("kategori", kategori.toMap());
+    return sonuc;
+  }
+
+  Future<int> kategoriGuncelle(Kategori kategori) async {
+    var db = await _getDatabase();
+    var sonuc = await db.update("kategori", kategori.toMap(),
+        where: 'kategoriID = ?', whereArgs: [kategori.kategoriID]);
+    return sonuc;
+  }
+
+  Future<int> kategoriSil(Kategori kategori) async {
+    var db = await _getDatabase();
+    var sonuc = await db.delete("kategori",
+        where: "kategoriID = ?", whereArgs: [kategori.kategoriID]);
+    return sonuc;
+  }
+
+  Future<List<Map<String, dynamic>>> notlariGetir() async {
+    var db = await _getDatabase();
+    var sonuc = await db.query("not", orderBy: 'notID DESC');
+    return sonuc;
+  }
+  // =====================================================================================================
+
+  // Not işlemleri
+  Future<int> notEkle(Not not) async {
+    var db = await _getDatabase();
+    var sonuc = await db.insert("not", not.toMap());
+    return sonuc;
+  }
+
+  Future<int> notGuncelle(Not not) async {
+    var db = await _getDatabase();
+    var sonuc = await db.update("not", not.toMap(),
+        where: 'notID = ?', whereArgs: [not.kategoriID]);
+    return sonuc;
+  }
+
+  Future<int> notSil(Not not) async {
+    var db = await _getDatabase();
+    var sonuc =
+        await db.delete("not", where: "notID = ?", whereArgs: [not.notID]);
     return sonuc;
   }
 }
