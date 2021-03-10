8a1acdb3edd618c85bf870ce5f44fc186945681c
mehmet berkay <atasoy182@gmail.com>
2021-03-10 08:17:26 +0300

    post bloc devam

--- infinite_list_with_bloc/lib/bloc/post_bloc.dart
+++ infinite_list_with_bloc/lib/bloc/post_bloc.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:http/http.dart' as http;
+import 'package:infinite_list_with_bloc/models/post.dart';
 import 'package:meta/meta.dart';
 
 import 'post_event.dart';
@@ -12,8 +15,47 @@ class PostBloc extends Bloc<PostEvent, PostState> {
 
   @override
   Stream<PostState> mapEventToState(PostEvent event) async* {
-    if (event is PostInitial) {
-      yield null;
+    final currentState = state;
+    if (event is PostFetched && !_hasReachedMax(currentState)) {
+      try {
+        if (currentState is PostInitial) {
+          final posts = await _fetchPosts(0, 20);
+          yield PostSuccess(posts: posts, hasReachedMax: false);
+          return;
+        }
+        if (currentState is PostSuccess) {
+          final posts = await _fetchPosts(currentState.posts.length, 20);
+          yield posts.isEmpty
+              ? currentState.copyWith(hasReachedMax: true)
+              : PostSuccess(
+                  posts: currentState.posts + posts,
+                  hasReachedMax: false,
+                );
+        }
+      } catch (_) {
+        yield PostFailure();
+      }
+    }
+  }
+
+  bool _hasReachedMax(PostState state) =>
+      state is PostSuccess && state.hasReachedMax;
+
+  Future<List<Post>> _fetchPosts(int startIndex, int limit) async {
+    final response = await httpClient.get(Uri(
+        path:
+            'https://jsonplaceholder.typicode.com/posts?_start=$startIndex&_limit=$limit'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body) as List;
+      return data.map((rawPost) {
+        return Post(
+          id: rawPost['id'],
+          title: rawPost['title'],
+          body: rawPost['body'],
+        );
+      }).toList();
+    } else {
+      throw Exception('error fetching posts');
     }
   }
 }
